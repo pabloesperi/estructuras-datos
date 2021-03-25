1836e1f06937a23efdaefa489c67fd12321b30ba
pabloesperi <pabloesperi@hotmail.com>
2021-03-25 10:23:26 +0100

    3r commi

--- src/queue/Queues.java
+++ src/queue/Queues.java
@@ -66,6 +66,29 @@ public class Queues {
 			System.out.println(cola.poll());
 		}
 		
+		System.out.println("-------------------------------");
+		
+		System.out.println("OBJETOS");
+		
+		//La clase no sabe cuál es la prioridad, con lo cual hay que indicarlo en la clase correspondiente. Esta clase tiene que implementar la 
+		//interfaz Comparable
+		
+		Queue <Persona> cola2 = new PriorityQueue<>();
+		
+		cola2.offer(new Persona("Pablo", "Esperi", 35));
+		cola2.offer(new Persona("Luis", "E", 36));		
+		cola2.offer(new Persona("P", "E", 34));
+		cola2.offer(new Persona("PP", "EE", 33));
+		
+		//Conocer el valor que está en el top
+		System.out.println("Cabeza: " + cola2.peek().getEdad());
+		
+		while(!cola2.isEmpty()) {
+			//con poll() se elimina el valor a la cabeza y se retorna.
+			System.out.println(cola2.poll().getEdad());
+		}
+		
+		
 	}
 
 }
